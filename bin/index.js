d5865c1c11771e6b21422cfba8c09bd5f386f54f
Tomas Hubelbauer <Tom@Tomass-MacBook-Air.local>
2017-12-25 18:19:06 +0100

    Implement canvas ripple demo

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,47 @@
+const module = {};
+let pointer;
+
 window.addEventListener('load', async event => {
-    const response = await fetch('index.wasm');
-    const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer, {});
-    console.log(wasm.instance.exports.add_one(1));
+  const response = await fetch('index.wasm');
+  const buffer = await response.arrayBuffer();
+  const wasm = await WebAssembly.instantiate(buffer, { env: { cos: Math.cos } });
+
+  module.alloc = wasm.instance.exports.alloc;
+  module.dealloc = wasm.instance.exports.dealloc;
+  module.fill = wasm.instance.exports.fill;
+
+  const canvas = document.getElementById('screen');
+  const width = canvas.getAttribute('width');
+  const height = canvas.getAttribute('height');
+  const context = canvas.getContext('2d');
+  const length = width * height * 4;
+
+  pointer = module.alloc(length);
+  const memory = new Uint8ClampedArray(wasm.instance.exports.memory.buffer, pointer, length);
+  const data = new ImageData(memory, width, height);
+
+  let start = null;
+
+  function step(timestamp) {
+    if (start === null) start = timestamp;
+    const progress = timestamp - start;
+    if (progress > 100) {
+      module.fill(pointer, width, height, timestamp);
+      start = timestamp;
+      window.requestAnimationFrame(draw);
+    } else {
+      window.requestAnimationFrame(step);
+    }
+  }
+
+  function draw() {
+    context.putImageData(data, 0, 0)
+    window.requestAnimationFrame(step);
+  }
+
+  window.requestAnimationFrame(step);
+});
+
+window.addEventListener('unload', event => {
+  module.dealloc(pointer);
 });
