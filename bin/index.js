9cfa255fbedcbad25fca34b0392b46823ac5f414
Tomas Hubelbauer <Tom@Tomass-MacBook-Air.local>
2017-12-25 18:57:28 +0100

    Simplify JS render logic and add FPS counter

--- bin/index.js
+++ bin/index.js
@@ -1,46 +1,29 @@
-const module = {};
+let module;
 let pointer;
 
 window.addEventListener('load', async event => {
   const response = await fetch('index.wasm');
-  const buffer = await response.arrayBuffer();
   const env = { }; // Pass math functions such as sin, cos, round, etc. `x: Math.x`.
-  const wasm = await WebAssembly.instantiate(buffer, { env });
-
-  module.alloc = wasm.instance.exports.alloc;
-  module.dealloc = wasm.instance.exports.dealloc;
-  module.fill = wasm.instance.exports.fill;
+  module = await WebAssembly.instantiate(await response.arrayBuffer(), { env });
 
   const canvas = document.getElementById('screen');
+  const context = canvas.getContext('2d');
   const width = canvas.getAttribute('width');
   const height = canvas.getAttribute('height');
-  const context = canvas.getContext('2d');
-  const length = width * height * 4;
+  const length = width * height * 4; // RGBA
+  pointer = module.instance.exports.alloc(length);
 
-  pointer = module.alloc(length);
-  const memory = new Uint8ClampedArray(wasm.instance.exports.memory.buffer, pointer, length);
+  const memory = new Uint8ClampedArray(module.instance.exports.memory.buffer, pointer, length);
   const data = new ImageData(memory, width, height);
 
-  let start = null;
-
-  function step(timestamp) {
-    if (start === null) start = timestamp;
-    const progress = timestamp - start;
-    if (progress > 100) {
-      module.fill(pointer, width, height, timestamp);
-      start = timestamp;
-      window.requestAnimationFrame(draw);
-    } else {
-      window.requestAnimationFrame(step);
-    }
-  }
-
-  function draw() {
-    context.putImageData(data, 0, 0)
+  let lastTimestamp;
+  window.requestAnimationFrame(function step(timestamp) {
+    module.instance.exports.fill(pointer, width, height, timestamp);
+    context.putImageData(data, 0, 0);
+    document.title = `${(1000 / (timestamp - lastTimestamp)).toFixed(2)} FPS`;
+    lastTimestamp = timestamp;
     window.requestAnimationFrame(step);
-  }
-
-  window.requestAnimationFrame(step);
+  });
 });
 
-window.addEventListener('unload', event => module.dealloc(pointer));
+window.addEventListener('unload', event => module.instance.exports.dealloc(pointer));
